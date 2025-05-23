3f59cb65da45d7b5ce366255808eef1399d2eeec
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com>
2025-05-23 12:23:22 -0500

    feat: :sparkles: data insertion

---
+++ datos.sql
@@ -0,0 +1,9 @@
+-- DML
+USE rrhh_agencia;
+
+INSERT INTO empleados (id, nombre, apellido, correo) VALUES
+(1, 'Ana', 'Ganbrie', 'ana.gabriel@gmail.com'),
+(2, 'Luis', 'Gomez', 'luisg@gmail.com'),
+(3, 'ramiro', 'Ramirez', 'ramiro.ramirez@gmail.com'),
+(4, 'Jorge', 'Martinez', 'jmartinez@gmail.com'),
+(5, 'Marta', 'Fernandez', 'mfernandez@gmail.com');
